cbe3b44e21f0d3f41d84bcceeba69daa5aa11d2f
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-01-06 14:03:48 +0900

    ✨ Section3を修了しました

---
+++ TypeScript/Section_3/dist/test.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("test");
+var namae = "satoh";
+//# sourceMappingURL=test.js.map
---
+++ TypeScript/Section_3/src/app.ts
@@ -0,0 +1,5 @@
+const button = document.querySelector("button")!; //エクスラクションマークでDOMに要素があることをダブルチェック
+
+button.addEventListener("click", () => {
+  console.log("CLICK");
+});
