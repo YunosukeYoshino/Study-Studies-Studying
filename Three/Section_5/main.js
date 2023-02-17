bd5d954f4d58abc75e44fa599d510b6cd33f3877
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-17 22:12:18 +0900

    ⚡️ ジオメトリを作ってみた

--- Three/Section_5/main.js
+++ Three/Section_5/main.js
@@ -1,23 +1,81 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-setupCounter(document.querySelector('#counter'))
+//シーン
+const scene = new THREE.Scene();
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(1, 1, 2);
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+/**
+ * ジオメトリを作ってみよう。
+ **/
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16)//width,height,depth
+
+
+//マテリアル
+const material = new THREE.MeshNormalMaterial({
+  // wireframe: true,
+});
+
+//メッシュ化
+const box = new THREE.Mesh(boxGeometry, material)
+scene.add(box)
+
+//ライト
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+//マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+window.addEventListener("resize", onWindowResize);
+
+const clock = new THREE.Clock();
+
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+  // console.log(elapsedTime);
+
+  //オブジェクトの回転
+  // sphere.rotation.x = elapsedTime;
+  // plane.rotation.x = elapsedTime;
+  // octahedron.rotation.x = elapsedTime;
+  // torus.rotation.x = elapsedTime;
+
+  // sphere.rotation.y = elapsedTime;
+  // plane.rotation.y = elapsedTime;
+  // octahedron.rotation.y = elapsedTime;
+
+  // torus.rotation.y = elapsedTime;
+
+  controls.update();
+
+  //レンダリング
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+//ブラウザのリサイズに対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+animate();
