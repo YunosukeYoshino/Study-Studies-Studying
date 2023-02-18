0045449b5f04146f127dfb24a944bd5e052f00c2
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-19 02:00:06 +0900

    Section5-36 バッファジオメトリを作成しました。

--- Three/Section_5/main.js
+++ Three/Section_5/main.js
@@ -29,8 +29,36 @@ const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 16)
 // const planeGeometry = new THREE.PlaneGeometry(1, 1)
 const planeGeometry = new THREE.PlaneGeometry(10, 10)
 const torusGeometry = new THREE.TorusGeometry(0.5, 0.2, 15, 123, Math.PI * 2)//引数調べて設定してあげる
+
+// バッファジオメトリー
+const bufferGeometry = new THREE.BufferGeometry();
+const positionArray = new Float32Array(9);
+/*
+浮動小数点 型付き配列 32ビットしか入らない
+型付き配列で型を指定してあげることによって。無駄な情報が入らない。
+*/
+
+//頂点の位置
+positionArray[0] = 0; //x座標
+positionArray[1] = 0; //y座標
+positionArray[2] = 0; //z座標
+
+positionArray[3] = 0;
+positionArray[4] = 1;
+positionArray[5] = 0;
+
+positionArray[6] = 1;
+positionArray[7] = 0;
+positionArray[8] = 0;
+
+console.log(positionArray);
+const positionAttritube = new THREE.BufferAttribute(positionArray, 3)
+
+bufferGeometry.setAttribute("position", positionAttritube)
+
 //マテリアル
-const material = new THREE.MeshNormalMaterial({
+// const material = new THREE.MeshNormalMaterial({
+const material = new THREE.MeshBasicMaterial({
   // wireframe: true,
 });
 
@@ -52,7 +80,10 @@ plane.position.y = -0.5//1に対して半分の位置を下げてあげる
 const Torus = new THREE.Mesh(torusGeometry, material)
 Torus.position.x = - 1.5
 Torus.position.y = 0.18
-scene.add(box, sphere, plane, Torus)
+// scene.add(box, sphere, plane, Torus)
+
+const buffer = new THREE.Mesh(bufferGeometry, material)
+scene.add(buffer)
 
 //ライト
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
