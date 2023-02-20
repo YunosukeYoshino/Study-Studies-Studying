7912b42da46720c2f4ccb5a89bfb467e012b9fb3
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-21 00:12:48 +0900

    ⚡️ Section12-91. ジオメトリを4つ作成して描画しよう終了しました。

--- Three/Section_12/main.js
+++ Three/Section_12/main.js
@@ -35,6 +35,33 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height); //レンダラーのサイズを指定
 renderer.setPixelRatio(window.devicePixelRatio);//ジオメトリーをくっきり映し出すことができる。
 
+/*
+オブジェクトを作成しよう
+*/
+
+//マテリアル
+const material = new THREE.MeshPhongMaterial({//金属っぽい質感のジオメトリー
+    color: "#3c94d7",
+    metalness: 0.86,
+    roughness: 0.37,//粗さ
+    flatShading: true
+})
+
+// ジオメトリー(メッシュ化したもの)
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material)//メッシュの中に直接ジオメトリーをかける。
+const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material)//メッシュの中に直接ジオメトリーをかける。
+const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16), material)//メッシュの中に直接ジオメトリーをかける。
+const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(), material)//メッシュの中に直接ジオメトリーをかける。
+
+// 回転用に配置する。
+mesh1.position.set(2, 0, 0);
+mesh2.position.set(-1, 0, 0);
+mesh3.position.set(2, 0, -6);
+mesh4.position.set(5, 0, 3);
+
+
+scene.add(mesh1, mesh2, mesh3, mesh4)
+
 renderer.render(scene, camera)
 
 
