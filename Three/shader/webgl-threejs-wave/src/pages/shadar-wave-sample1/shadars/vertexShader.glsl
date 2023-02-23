9764aee355c18067ad9d80077b1ec442f9fe6913
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-24 03:19:26 +0900

    Section6-32 glslで角度を波のような表現

--- Three/shader/webgl-threejs-wave/src/pages/shadar-wave-sample1/shadars/vertexShader.glsl
+++ Three/shader/webgl-threejs-wave/src/pages/shadar-wave-sample1/shadars/vertexShader.glsl
@@ -1,3 +1,13 @@
+uniform float uWaveLength;
+
 void main(){
-     gl_Position = projectionMatrix * modelMatrix * viewMatrix * vec4(position,1.0);
+     vec4 modelPosition = modelMatrix * vec4(position,1.0);
+
+     modelPosition.y += sin(modelPosition.x)* uWaveLength; //波のような表現
+
+
+     vec4 viewPosition = viewMatrix * modelPosition;
+     vec4 projectionMatrix = projectionMatrix * viewPosition;
+
+     gl_Position = projectionMatrix;
 }
