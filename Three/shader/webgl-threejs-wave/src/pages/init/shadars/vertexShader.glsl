71528e7fe746cf99192a9868ebe09afa45049537
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-03-04 00:25:26 +0900

    ⚡️ vertexShaderについて学んだ！

--- Three/shader/webgl-threejs-wave/src/pages/init/shadars/vertexShader.glsl
+++ Three/shader/webgl-threejs-wave/src/pages/init/shadars/vertexShader.glsl
@@ -3,12 +3,13 @@
 */
 
 // mat4 4*4の行列
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform mat4 projectionMatrix;//カメラのす範囲を決める 座標変換
+uniform mat4 viewMatrix; //カメラの位置や方角を決めるための行列
+uniform mat4 modelMatrix;//物体の位置の情報
 
 attribute vec3 position;//x y zの座標
 
+//vec4 4次元ベクトル
 void main() {
      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
