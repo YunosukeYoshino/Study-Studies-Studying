99f1e4d1b4935cda2adb08bc291e2b2810768d2d
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2023-03-06 02:05:43 +0900

    ⚡️ 旗に影をつけました。

--- Three/shader/webgl-threejs-wave/src/pages/frag-shader/shadars/fragmentShader.glsl
+++ Three/shader/webgl-threejs-wave/src/pages/frag-shader/shadars/fragmentShader.glsl
@@ -2,6 +2,7 @@ uniform vec3 uColor; // r g gは３次元ベクトルのためvec3を宣言
 uniform sampler2D uTexture;//textureをつけるときに型を宣言しなければならない
 
 varying vec2 vUv;//uv座標は元々用意されている。
+varying float VElevation;//elavationを定義
 
 /**
 * textureを割り当てるためには、uv座標が必要
@@ -10,5 +11,6 @@ varying vec2 vUv;//uv座標は元々用意されている。
 void main() {
 	vec4 textureColor = texture2D(uTexture, vUv);
 	// gl_FragColor = vec4(uColor, 1.0);//RGBA
+	textureColor.rgb *= VElevation * 2.9 + 0.7;
 	gl_FragColor = textureColor;
 }
--- Three/shader/webgl-threejs-wave/src/pages/frag-shader/shadars/vertexShader.glsl
+++ Three/shader/webgl-threejs-wave/src/pages/frag-shader/shadars/vertexShader.glsl
@@ -2,6 +2,7 @@ uniform vec2 uFrequency; //Vector2にしたのでvec2型に変更
 uniform float uTime;
 
 varying vec2 vUv;//uv座標は元々用意されている。
+varying float VElevation;//elavationを定義
 
 void main() {
      // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
@@ -13,12 +14,17 @@ void main() {
      *sin関数：波のような形
      *かけることによって周波数を上げる
      */
-     modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-     modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+     // modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+     // modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+
+     // 変数として切り出すためにelevationを定義する
+     float elevation = sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+     elevation += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+     modelPosition.z += elevation;
 
      vec4 viewPosition = viewMatrix * modelPosition;
      vec4 projectionPosition = projectionMatrix * viewPosition;
      gl_Position = projectionPosition;
      vUv = uv;
-
+     VElevation += elevation;
 }
